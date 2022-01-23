65565fa445e02a2037f9adeec232bdd5cdf13ca9
czak <czak@earth>
2022-01-23 23:31:16 +0100

    Starting init_display

--- src/native-state-webos.cpp
+++ src/native-state-webos.cpp
@@ -1,16 +1,90 @@
 #include "native-state-webos.h"
 
-NativeStateWebos::NativeStateWebos()
+#include <cstring>
+#include <csignal>
+
+const struct wl_registry_listener NativeStateWebos::registry_listener_ = {
+    NativeStateWebos::registry_handle_global,
+    NativeStateWebos::registry_handle_global_remove
+};
+
+void
+NativeStateWebos::registry_handle_global(void *data, struct wl_registry *registry,
+                                         uint32_t id, const char *interface,
+                                         uint32_t version)
+{
+    NativeStateWebos *that = static_cast<NativeStateWebos *>(data);
+    if (strcmp(interface, "wl_compositor") == 0) {
+        that->display_->compositor =
+                static_cast<struct wl_compositor *>(
+                    wl_registry_bind(registry,
+                                     id, &wl_compositor_interface, std::min(version, 4U)));
+    }
+}
+
+void
+NativeStateWebos::registry_handle_global_remove(void * /*data*/,
+                                                struct wl_registry * /*registry*/,
+                                                uint32_t /*name*/)
+{
+}
+
+NativeStateWebos::NativeStateWebos() : display_(0), window_(0)
 {
 }
 
 NativeStateWebos::~NativeStateWebos()
 {
+    if (window_) {
+        if (window_->native)
+            wl_egl_window_destroy(window_->native);
+        if (window_->surface)
+            wl_surface_destroy(window_->surface);
+        delete window_;
+    }
+
+    if (display_) {
+        if (display_->compositor)
+            wl_compositor_destroy(display_->compositor);
+        if (display_->registry)
+            wl_registry_destroy(display_->registry);
+        if (display_->display) {
+            wl_display_flush(display_->display);
+            wl_display_disconnect(display_->display);
+        }
+        delete display_;
+    }
 }
 
 bool
 NativeStateWebos::init_display()
 {
+    struct sigaction sa;
+    sa.sa_handler = &NativeStateWebos::quit_handler;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+
+    display_ = new struct my_display();
+
+    if (!display_) {
+        return false;
+    }
+
+    display_->display = wl_display_connect(NULL);
+
+    if (!display_->display) {
+        return false;
+    }
+
+    display_->registry = wl_display_get_registry(display_->display);
+
+    wl_registry_add_listener(display_->registry, &registry_listener_, this);
+
+    wl_display_roundtrip(display_->display);
+
     return true;
 }
 
@@ -46,4 +120,10 @@ NativeStateWebos::should_quit()
 void
 NativeStateWebos::flip()
 {
+}
+
+void
+NativeStateWebos::quit_handler(int /*signum*/)
+{
+    should_quit_ = true;
 }
--- src/native-state-webos.h
+++ src/native-state-webos.h
@@ -1,6 +1,9 @@
 #ifndef GLMARK2_NATIVE_STATE_WEBOS_H_
 #define GLMARK2_NATIVE_STATE_WEBOS_H_
 
+#include <wayland-client.h>
+#include <wayland-egl.h>
+
 #include "native-state.h"
 
 class NativeStateWebos : public NativeState
@@ -16,6 +19,33 @@ public:
     void visible(bool v);
     bool should_quit();
     void flip();
+
+private:
+    static void quit_handler(int signum);
+
+    static const struct wl_registry_listener registry_listener_;
+
+    static void
+    registry_handle_global(void *data, struct wl_registry *registry,
+                           uint32_t id, const char *interface,
+                           uint32_t version);
+    static void
+    registry_handle_global_remove(void *data, struct wl_registry *registry,
+                                  uint32_t name);
+
+    struct my_display {
+        wl_display *display;
+        wl_registry *registry;
+        wl_compositor *compositor;
+    } *display_;
+
+    struct my_window {
+        WindowProperties properties;
+        struct wl_surface *surface;
+        struct wl_egl_window *native;
+    } *window_;
+
+    static volatile bool should_quit_;
 };
 
 #endif /* GLMARK2_NATIVE_STATE_WEBOS_H_ */
